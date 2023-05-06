6f0eec17f07ac7843a1dc1544f47c8ff7ec0e95c
Brahma <115888369+BrahmaCh9010@users.noreply.github.com>
2023-05-06 14:15:18 +0530

    spring-boots-task

---
+++ spring-boot-tasks/src/main/java/com/springtasks/demo/SpringBootTasksApplication.java
@@ -0,0 +1,16 @@
+package com.springtasks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTasksApplication.class, args);
+		
+		System.out.println("application run ssucess");
+	}
+	
+
+}
---
+++ spring-boot-tasks/src/test/java/com/springtasks/demo/SpringBootTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtasks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
