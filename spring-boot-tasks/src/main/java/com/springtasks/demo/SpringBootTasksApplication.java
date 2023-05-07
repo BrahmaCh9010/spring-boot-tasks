2c8dc8c4f6fe39cdb6a8165898cbdb5baff497ac
Brahma <115888369+BrahmaCh9010@users.noreply.github.com>
2023-05-07 09:21:59 +0530

    spring ioc container

--- spring-boot-tasks/src/main/java/com/springtasks/demo/SpringBootTasksApplication.java
+++ spring-boot-tasks/src/main/java/com/springtasks/demo/SpringBootTasksApplication.java
@@ -2,13 +2,19 @@ package com.springtasks.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringBootTasksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTasksApplication.class, args);
-		
+		//SpringApplication.run(SpringBootTasksApplication.class, args);
+		ApplicationContext context =
+				 new ClassPathXmlApplicationContext("Beans.xml");
+				 HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+				 obj.getMessage();
+
 		System.out.println("application run ssucess");
 	}
 	
