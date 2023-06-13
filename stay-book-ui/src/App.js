0dc03d78d65e09c5933b4dae9f8e811aebc9898a
Brahma <bathulabrahmaiah@gmail.com>
2023-06-13 15:36:54 +0530

    create ui

---
+++ stay-book-ui/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Brahma</h2>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
