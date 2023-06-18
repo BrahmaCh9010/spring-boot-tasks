e753e2cdeadeb8229a3630dc55f5e9c4090fc5e1
Brahma <bathulabrahmaiah@gmail.com>
2023-06-18 11:57:37 +0530

    starpg app

---
+++ starpg/src/main/java/com/starpg/StarpgApplication.java
@@ -0,0 +1,13 @@
+package com.starpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarpgApplication.class, args);
+	}
+
+}
---
+++ starpg/src/test/java/com/starpg/StarpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
