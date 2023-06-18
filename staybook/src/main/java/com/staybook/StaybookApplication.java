7b811de92c44a9273a90f7ef84ab96481674f190
Brahma <bathulabrahmaiah@gmail.com>
2023-06-18 20:27:42 +0530

    staybook

--- staybook/src/main/java/com/staybook/StaybookApplication.java
+++ staybook/src/main/java/com/staybook/StaybookApplication.java
@@ -3,7 +3,11 @@ package com.staybook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class StaybookApplication {
 
 	public static void main(String[] args) {
